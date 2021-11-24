d959e95d8bc818ad435b73635e74570e481348c9
chipolito <60354138+chipolito@users.noreply.github.com>
2021-11-24 15:04:02 -0600

    invoice
    Se lista todas las facturas
    se puede accedeer al detalle de cada factura
    
    se procede a hablitar la edicion de facturas

--- core/models/invoice.php
+++ core/models/invoice.php
@@ -27,4 +27,41 @@
 		        return FALSE;
 		    }
 		}
+
+		public function getInvoice(){
+			$pdo = new Conexion();
+			$cmd = '
+				SELECT 
+					id,
+					client_id, 
+					(select concat(nombre, " ", apellido) from client where id = client_id) AS clientName,
+					detalles, 
+					importe, 
+					fecha, 
+					estatus
+				FROM invoice
+				WHERE activo = 1;
+			';
+			
+			$sql = $pdo->prepare($cmd);
+			$sql->execute();
+
+			return $sql->fetchAll(PDO::FETCH_ASSOC);
+		}
+
+		public function deleteInvoice($invoiceId){
+			$pdo = new Conexion();
+			$cmd = '
+				UPDATE invoice SET activo = 0 WHERE id =:invoiceId;
+			';
+
+			$parametros = array(
+				':invoiceId' => $invoiceId
+			);
+			
+			$sql = $pdo->prepare($cmd);
+			$sql->execute($parametros);
+
+			return TRUE;
+		}
 	}
