66cc4dcde73bb8a089adbaadedd07b59642e504d
chipolito <60354138+chipolito@users.noreply.github.com>
2022-01-24 14:02:31 -0600

    update master page
    Se añadio la librerio de bootstrap JS no se estaba cargando y no funcionaban secciones de la ágina

--- masterPage.php
+++ masterPage.php
@@ -181,6 +181,9 @@
     </div>
     <!-- End formulario para el chat -->
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     <!-- Masterpage JS File -->
     <script type="text/javascript">
         var base_url = "<?php echo $base_url; ?>"
